155ce4df5dfd0eaf7c5c6e4804f2b2c322dacbff
LaionSilva <55113565+LaionSilva@users.noreply.github.com>
2019-10-13 00:12:11 -0300

    v0.1

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace logistica {
+  class MainClass {
+    public static void Main (string[] args) {
+      Console.WriteLine ("\nParabéns! O código esta copilando");
+    }
+  }
+}
